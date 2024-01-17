3fe606e61d62396195cf33ff7b7804a60edf16ac
varshith-el <varshithel15@gmail.com>
2024-01-17 11:42:51 +0530

    Create Chat Channel

---
+++ chat/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+from django.shortcuts import render, redirect
+ 
+ 
+def chatPage(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("login-user")
+    context = {}
+    return render(request, "chat/chatPage.html", context)
